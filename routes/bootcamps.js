e53bb46a5a27f82a81f0117408ced11ec580ebd6
Minh Truong <truongcongminh96@gmail.com>
2021-03-19 14:26:40 +0700

    Geospatial Query - Get Bootcamps Within Radius

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const {
-    getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp
+    getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius
 } = require('../controllers/bootcamps');
 
 const router = express.Router();
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/').get(getBootcamps).post(createBootcamp);
 
