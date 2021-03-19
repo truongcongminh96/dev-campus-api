090be720ea7c5b5b7c18fc8c6d8819638357f1f1
Minh Truong <truongcongminh96@gmail.com>
2021-03-19 10:11:50 +0700

    Mongoose Error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,7 +9,7 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps = await Bootcamp.find();
         res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
     } catch (e) {
-        res.status(400).json({success: false});
+        next(e);
     }
 };
 
@@ -26,8 +26,7 @@ exports.getBootcamp = async (req, res, next) => {
 
         res.status(200).json({success: true, data: bootcamp});
     } catch (e) {
-        //res.status(400).json({success: false});
-        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        next(e);
     }
 };
 
@@ -42,7 +41,7 @@ exports.createBootcamp = async (req, res, next) => {
         }
         res.status(201).json({success: true, data: bootcamp});
     } catch (e) {
-        res.status(400).json({success: false});
+        next(e);
     }
 };
 
@@ -57,12 +56,12 @@ exports.updateBootcamp = async (req, res, next) => {
         });
 
         if (!bootcamp) {
-            return res.status(400).json({success: false});
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({success: true, data: bootcamp});
     } catch (e) {
-        return res.status(400).json({success: false});
+        next(e);
     }
 };
 
@@ -74,11 +73,11 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({success: false});
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({success: true, data: {}});
     } catch (e) {
-        return res.status(400).json({success: false});
+        next(e);
     }
 };
