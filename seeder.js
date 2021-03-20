a12ec4c3eb7fb2ab6dbc28d70bf16bb0abeb0082
Truong Minh <truongcongminh96@gmail.com>
2021-03-20 21:05:37 +0700

    Course Model & Seeding

--- seeder.js
+++ seeder.js
@@ -4,46 +4,55 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 
 // Load env vars
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // Read JSON files
-const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
-    try {
-        await Bootcamp.create(bootcamps);
-        console.log('Data Imported...'.green.inverse);
-        process.exit();
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 // Delete data
 const deleteData = async () => {
-    try {
-        await Bootcamp.deleteMany();
-        console.log('Data Destroyed...'.red.inverse);
-        process.exit();
-    } catch (e) {
-        console.error(e);
-    }
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 if (process.argv[2] === '-i') {
-    importData().then();
+  importData();
 } else if (process.argv[2] === '-d') {
-    deleteData().then();
+  deleteData();
 }
