93cf4f8bc9fcbd50bda89d74bccb882e8ac090af
Minh Truong <truongcongminh96@gmail.com>
2021-03-22 13:14:32 +0700

    Advanced Results Middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -3,6 +3,9 @@ const {
     getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius, bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
+
 const router = express.Router();
 
 // Include other resource routers
@@ -15,7 +18,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
