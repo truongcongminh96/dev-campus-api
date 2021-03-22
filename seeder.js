6ba9b2a60676a6e8848fb5d021a626b621a4e1f5
Minh Truong <truongcongminh96@gmail.com>
2021-03-22 16:18:50 +0700

    Bootcamp & User Relationship

--- seeder.js
+++ seeder.js
@@ -4,55 +4,62 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 
 // Load env vars
-dotenv.config({ path: './config/config.env' });
+dotenv.config({path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 // Read JSON files
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+    fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
 const courses = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+    fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
 // Import into DB
 const importData = async () => {
-  try {
-    await Bootcamp.create(bootcamps);
-    await Course.create(courses);
-    console.log('Data Imported...'.green.inverse);
-    process.exit();
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        await Bootcamp.create(bootcamps);
+        await Course.create(courses);
+        await User.create(users);
+        console.log('Data Imported...'.green.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 // Delete data
 const deleteData = async () => {
-  try {
-    await Bootcamp.deleteMany();
-    await Course.deleteMany();
-    console.log('Data Destroyed...'.red.inverse);
-    process.exit();
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        await Bootcamp.deleteMany();
+        await Course.deleteMany();
+        await User.deleteMany();
+        console.log('Data Destroyed...'.red.inverse);
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 if (process.argv[2] === '-i') {
-  importData().then();
+    importData().then();
 } else if (process.argv[2] === '-d') {
-  deleteData().then();
+    deleteData().then();
 }
