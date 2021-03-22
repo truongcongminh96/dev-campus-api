9a08677219f11b8cc2bcfaa61daf55373b07bae6
Minh Truong <truongcongminh96@gmail.com>
2021-03-22 14:08:06 +0700

    Sign & Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,5 +13,8 @@ exports.register = asyncHandler(async (req, res, next) => {
         name, email, password, role
     });
 
-    res.status(200).json({success:true});
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token});
 });
