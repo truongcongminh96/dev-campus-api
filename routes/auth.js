f3225d00c220a6b5174eb32dc869905da95ede1b
Minh Truong <truongcongminh96@gmail.com>
2021-03-23 10:41:18 +0700

    Update User Details

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, getMe, forgotPassword, resetPassword} = require('../controllers/auth');
+const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controllers/auth');
 const {protect} = require('../middleware/auth');
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/update_details', protect, updateDetails);
+router.put('/update_password', protect, updatePassword);
 router.post('/forgot_password', forgotPassword);
 router.put('/reset_password/:resetToken', resetPassword);
 
