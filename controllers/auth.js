d0dd7071d96c9d1b943668e8e8bd8c325172d3a9
Minh Truong <truongcongminh96@gmail.com>
2021-03-23 10:25:57 +0700

    Reset Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
+const crypto = require('crypto');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc      Register user
@@ -39,6 +41,31 @@ exports.login = asyncHandler(async (req, res, next) => {
     if (!isMatch) {
         return next(new ErrorResponse('Invalid credentials', 401));
     }
+
+    sendTokenResponse(user, 200, res);
+});
+
+// @desc      Reset password
+// @route     PUT /api/v1/auth/reset_password/:resetToken
+// @access    Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+    // Get hashed token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: {$gt: Date.now()}
+    });
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid token', 400));
+    }
+
+    // Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
 
     sendTokenResponse(user, 200, res);
 });
@@ -89,6 +116,29 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     const resetToken = user.getResetPasswordToken();
 
     await user.save({validateBeforeSave: false});
+
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/reset_password/${resetToken}`;
+
+    const message = `You're receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({success: true, data: 'Email sent'});
+    } catch (e) {
+        console.log(e);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordToken = undefined;
+
+        await user.save({validateBeforeSave: false});
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+    }
 
     res.status(200).json({
         success: true,
---
+++ utils/sendEmail.js
@@ -0,0 +1,36 @@
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+const sendEmail = async (options) => {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.SMTP_EMAIL, // generated ethereal user
+            pass: process.env.SMTP_PASSWORD, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    const message = {
+        from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`, // sender address
+        to: options.email, // list of receivers
+        subject: options.subject, // Subject line
+        text: options.message, // plain text body
+        // html: "<b>Hello world?</b>", // html body
+    };
+
+    const info = await transporter.sendMail(message);
+
+    console.log("Message sent: %s", info.messageId);
+};
+
+module.exports = sendEmail;
+
